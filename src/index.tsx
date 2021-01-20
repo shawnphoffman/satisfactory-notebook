ec74dac8d3216e37ded12daacc1b13a8b6d9a7da
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2021-01-20 12:23:59 -0800

    Add Sentry logging

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,22 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+Sentry.init({
+	dsn: process.env.REACT_APP_SENTRY_DSN,
+	autoSessionTracking: true,
+	integrations: [new Integrations.BrowserTracing()],
+
+	// We recommend adjusting this value in production, or using tracesSampler
+	// for finer control
+	tracesSampleRate: 1.0,
+})
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
